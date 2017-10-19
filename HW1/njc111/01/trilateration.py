6a5a2ec8f7fa15c7d891ba4dab24b8066c7f6f48
unshure <nac542@gmail.com>
2017-10-19 13:02:23 -0400

    fixing stuff

--- HW1/njc111/01/trilateration.py
+++ HW1/njc111/01/trilateration.py
@@ -1,42 +1,54 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+import warnings
 
 def trilaterate3D(distances):
-    p1 = np.array(distances[0][0:3])
-    p2 = np.array(distances[1][0:3])
-    p3 = np.array(distances[2][0:3])
-    p4 = np.array(distances[3][0:3])
-    r1 = distances[0][3]
-    r2 = distances[1][3]
-    r3 = distances[2][3]
-    r4 = distances[3][3]
-    x,y,z = 0,0,0
+    pt1 = np.array(distances[0])
+    pt2 = np.array(distances[1])
+    pt3 = np.array(distances[2])
+    pt4 = np.array(distances[3])
+    lst = itertools.permutations([pt1,pt2,pt3,pt4],4)
+    avg = []
+    for points in lst:
+        p1 = points[0][0:3]
+        p2 = points[1][0:3]
+        p3 = points[2][0:3]
+        p4 = points[3][0:3]
+        r1 = points[0][3]
+        r2 = points[1][3]
+        r3 = points[2][3]
+        r4 = points[3][3]
+        x,y,z = 0,0,0
 
-    ex = (p2-p1)/(np.linalg.norm(p2-p1))
-    i =  np.dot(ex, (p3-p1))
+        ex = (p2-p1)/(np.linalg.norm(p2-p1))
+        i =  np.dot(ex, (p3-p1))
 
-    ey = (p3-p1 -i*ex)/(np.linalg.norm(p3-p1-i*ex))
+        ey = (p3-p1 -i*ex)/(np.linalg.norm(p3-p1-i*ex))
 
-    ez = np.cross(ex,ey)
+        ez = np.cross(ex,ey)
 
-    d = np.linalg.norm(p2-p1)
-    # cj was chosed instad of j since j represents complex numbers
-    cj = np.dot(ey,(p3-p1))
+        d = np.linalg.norm(p2-p1)
+        # cj was chosed instad of j since j represents complex numbers
+        cj = np.dot(ey,(p3-p1))
 
-    x = (np.power(r1,2) - np.power(r2,2) + np.power(d,2))/(2*d)
-    y = (np.power(r1,2)-np.power(r3,2)+np.power(i,2)+np.power(cj,2))/(2*cj) - (i*x)/(cj)
-    zPos = np.sqrt(np.power(r1,2)-np.power(x,2)-np.power(y,2))
-    zNeg = -1*np.sqrt(np.power(r1,2)-np.power(x,2)-np.power(y,2))
+        x = (np.power(r1,2) - np.power(r2,2) + np.power(d,2))/(2*d)
+        y = (np.power(r1,2)-np.power(r3,2)+np.power(i,2)+np.power(cj,2))/(2*cj) - (i*x)/(cj)
+        zPos = np.sqrt(abs(np.power(r1,2)-np.power(x,2)-np.power(y,2)))
+        zNeg = -1*np.sqrt(abs(np.power(r1,2)-np.power(x,2)-np.power(y,2)))
 
-    pPos = p1 + x*ex + y*ey + zPos*ez
-    pNeg = p1 + x*ex + y*ey + zNeg*ez
 
-    if abs(np.linalg.norm(abs(p4-pPos)) -r4) > abs(np.linalg.norm(abs(p4-pNeg)) -r4):
-        return pPos
-    else:
-        return pNeg
 
+        pPos = p1 + x*ex + y*ey + zPos*ez
+        pNeg = p1 + x*ex + y*ey + zNeg*ez
+
+        if abs(np.linalg.norm(abs(p4-pPos)) -r4) > abs(np.linalg.norm(abs(p4-pNeg)) -r4):
+            avg.append(pNeg)
+        else:
+            avg.append(pPos)
+    print  avg[3][:]
+    return sum(avg)/len(avg)
 
 if __name__ == "__main__":
     
